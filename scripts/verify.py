0c69f5234c58c1ceb8309a19e844abe26eacf76f
James Turk <james.p.turk@gmail.com>
2013-05-14 21:53:54 -0400

    fix ascii-only bug

--- scripts/verify.py
+++ scripts/verify.py
@@ -9,7 +9,7 @@ import argparse
 import collections
 
 
-VALID_ID = re.compile('^ocd-division/country:\w\w(/\w+:[0-9A-Za-z._~-]+)*$')
+VALID_ID = re.compile('^ocd-division/country:\w\w(/\w+:[\w\d._~-]+)*$')
 
 
 if __name__ == '__main__':
