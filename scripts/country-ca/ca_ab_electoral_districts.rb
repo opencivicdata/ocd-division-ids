f7a64130504504d0526f3509c92237a20a3669ff
James McKinney <james@slashpoundbang.com>
2013-06-03 19:09:43 -0400

    Add identifiers and mappings for Alberta provincial electoral districts

---
+++ scripts/country-ca/ca_ab_electoral_districts.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require File.expand_path(File.join('..', 'utils.rb'), __FILE__)
+
+# Scrapes Alberta electoral district codes and names from altalis.com
+
+class AB < Runner
+  @csv_filename = "province-ab-electoral_districts.csv"
+
+  def identifiers
+    ShapefileParser.new(
+      "http://altalis.com/Samples/Provincial%20Electoral%20Divisions%20Current%202010.zip",
+      "ocd-division/country:ca/province:ab/ped:", {
+        :identifier => "EDNUMBER",
+        :name => "EDNAME",
+      }
+    ).run
+  end
+end
+
+AB.new.run(ARGV)
