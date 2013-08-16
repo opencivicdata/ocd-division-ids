18eebdfdee6a5a4724fab07adcb1df18d151a88b
James McKinney <james@slashpoundbang.com>
2013-08-15 23:54:58 -0400

    Add more comments, fix CSV file extension

--- scripts/country-ca/ca_census_subdivision_urls.rb
+++ scripts/country-ca/ca_census_subdivision_urls.rb
@@ -11,15 +11,18 @@ class OpenCivicDataFile < Array
   class << self
     attr_reader :directory
 
+    # Reads a local CSV file.
     def read(path)
       new(CSV.read(File.expand_path(File.join("..", "..", "..", directory, path + ".csv"), __FILE__)))
     end
   end
 
+  # Transforms the OCD identifiers to type IDs.
   def abbreviate!
     replace(map{|identifier,value| [identifier[/[^:]+\z/], value]})
   end
 
+  # Transforms the two-column CSV to a hash.
   def to_h
     Hash[*flatten]
   end
@@ -37,19 +40,23 @@ class DivisionName < String
   class << self
     attr_reader :divisions, :identifier_mappings, :name_mappings, :type_mappings, :type_patterns
 
+    # Returns the OCD identifier for the name.
     def identifier_from_name(name)
       identifier_mappings[name]
     end
   end
 
+  # Removes the census subdivision type from the division name.
   def remove_type(province_or_territory)
     sub(pattern(province_or_territory), "")
   end
 
+  # Returns whether the division name contains a census subdivision type.
   def has_type?(province_or_territory)
     !!self[pattern(province_or_territory)]
   end
 
+  # Returns the census subdivision type code.
   def type(province_or_territory)
     matches = match(pattern(province_or_territory))
     mapping = type_mappings.fetch(province_or_territory)
@@ -58,10 +65,13 @@ class DivisionName < String
 
 private
 
+  # Returns the regular expression for capturing a census subdivision type in a
+  # particular province or territory.
   def pattern(province_or_territory)
     type_patterns.fetch(province_or_territory)
   end
 
+  # Returns the scraped name, an alternate name, or the official name.
   def alternate_or_self
     if identifier_mappings.key?(self)
       self.class.new(divisions.fetch(identifier_mappings[self]))
@@ -72,22 +82,32 @@ private
     end
   end
 
+  # Returns a hash from OCD identifier to official name.
   def divisions
     self.class.divisions
   end
 
+  # Returns a hash from a scraped name to an OCD identifier. Used in cases where
+  # the scraped name has no resemblance to the official name.
   def identifier_mappings
     self.class.identifier_mappings
   end
 
+  # Returns a hash from a scraped name to an alternate name. Used in cases where
+  # the scraped name has a typo or major differences from the official name.
   def name_mappings
     self.class.name_mappings
   end
 
+  # Returns a hash in which the keys are province or territory type IDs and the
+  # values are hashes in which the keys are census subdivision type names and
+  # the values are census subdivision type codes.
   def type_mappings
     self.class.type_mappings
   end
 
+  # Returns a hash in which the keys are province or territory type IDs and the
+  # values are regular expressions for capturing a census subdivision type.
   def type_patterns
     self.class.type_patterns
   end
@@ -136,13 +156,13 @@ class CensusSubdivisionName < DivisionName
 
   @identifier_mappings = {
     # http://en.wikipedia.org/wiki/Redwood_Meadows,_Alberta
-    "Townsite of Redwood Meadows" => "ocd-division/country:ca/csd:4806804",
+    "Townsite of Redwood Meadows" => "ocd-division/country:ca/csd:4806804", # Tsuu T'ina Nation 145 (Sarcee 145)
     # http://woodypoint.ca/
-    "Town of Woody Point"         => "ocd-division/country:ca/csd:1009011",
+    "Town of Woody Point"         => "ocd-division/country:ca/csd:1009011", # Woody Point, Bonne Bay
     # http://www.gov.pe.ca/placefinder/index.php3?city=North%20Shore
-    "Community of North Shore"    => "ocd-division/country:ca/csd:1102052",
+    "Community of North Shore"    => "ocd-division/country:ca/csd:1102052", # Lot 34
     # http://www.gov.pe.ca/placefinder/index.php3?city=St.%20Nicholas
-    "Community of St. Nicholas"   => "ocd-division/country:ca/csd:1103019",
+    "Community of St. Nicholas"   => "ocd-division/country:ca/csd:1103019", # Lot 17
   }
 
   @name_mappings = {
@@ -334,24 +354,28 @@ class CensusSubdivisionIdentifier < String
     attr_reader :census_subdivision_types, :province_and_territory_sgc_codes
   end
 
+  # Returns the province or territory's type ID (also its postal abbreviation).
   def province_or_territory_type_id
     self.class.province_and_territory_sgc_codes.fetch(province_or_territory_sgc_code)
   end
 
+  # Returns the province or territory SGC code, e.g. "24", which is the first
+  # two digits of the census subdivision SGC code.
   def province_or_territory_sgc_code
     self[/[^:]+\z/][0,2]
   end
 
+  # Returns the census division type ID (also its SGC code), which is the first
+  # four digits of the census subdivision SGC code.
   def census_division_type_id
     self[/[^:]+\z/][0,4]
   end
 
+  # Returns the census subdivision's type.
   def census_subdivision_type
     self.class.census_subdivision_types.fetch(self).sub(/\ATV\z/, "T").sub(/\AC\z/, "CY")
   end
 end
-
-
 
 province_or_territory_map = lambda do |(_,name)|
   # Effective October 20, 2008, the name "Yukon Territory" became "Yukon". The
@@ -363,6 +387,8 @@ end
 provinces_and_territories = OpenCivicDataIdentifiers.read("country-ca/ca_provinces_and_territories")
 provinces_and_territories_hash = Lycopodium.new(provinces_and_territories, province_or_territory_map).value_to_fingerprint.invert
 
+# `value` is either a OCD identifier or a province or territory type ID. `name`
+# is a either an official or a scraped census subdivision name.
 census_subdivision_map = lambda do |(value,name)|
   value = CensusSubdivisionName.identifier_from_name(name) || value
   name = CensusSubdivisionName.new(name).normalize
