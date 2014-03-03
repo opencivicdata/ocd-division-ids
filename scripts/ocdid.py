f52b7dae3a6681b1d2c41cfeb6d6665225aaf3c0
James Michael DuPont <jamesmikedupont@gmail.com>
2014-03-02 19:24:31 -0600

    flake8

--- scripts/ocdid.py
+++ scripts/ocdid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import requests
-from fuzzywuzzy import fuzz,process
+from fuzzywuzzy import fuzz, process
 from operator import itemgetter
 
 """
@@ -21,34 +21,47 @@ MATCH_LIMIT -- maximum number of matched values returned
 SEARCH_CONVERSIONS -- conversions for district search types to valid ocd types
 
 """
-OCDID_DATA = 'https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-us.csv'
-EXCEPTION_DATA = 'https://raw.github.com/opencivicdata/ocd-division-ids/master/identifiers/country-us/exceptions.csv'
-MATCH_RATIO = 90 
+BASE = 'https://raw.github.com/opencivicdata/ocd-division-ids'
+OCDID_DATA = BASE + '/master/identifiers/country-us.csv'
+EXCEPTION_DATA = BASE + '/master/identifiers/country-us/exceptions.csv'
+MATCH_RATIO = 90
 MATCH_LIMIT = 10
 SEARCH_CONVERSIONS = {
-                'city':set(['place','district']),
-                'town':set(['place']),
-                'township':set(['place']),
-                'village':set(['place']),
-                'county':set(['county','parish','census_area','borough','region']),
-                'parish':set(['county','parish','census_area','borough','region']),
-                'census_area':set(['county','parish','census_area','borough','region']),
-                'borough':set(['county','parish','census_area','borough','region']),
-                'region':set(['county','parish','census_area','borough','region']),
-                'state':set(['state','territory','district']),
-                'territory':set(['state','territory','district']),
-                'council':set(['council_district','commissioner_district']),
-                'commissioner':set(['council_district','commissioner_district']),
-                'court':set(['chancery_court','superior_court','supreme_court','court_of_appeals','district_court','circuit_court','constable_districts']),
-                'school':set(['school_board']),
-                'ward':set(['anc','ward','precinct']),
-                'anc':set(['anc','ward','precinct']),
-                'precinct':set(['anc','ward','precinct']),
-                'country':set(['country']),
-                'cd':set(['cd']),
-                'sldl':set(['sldl']),
-                'sldu':set(['sldu'])}
-                
+    'city': set(['place', 'district']),
+    'town': set(['place']),
+    'township': set(['place']),
+    'village': set(['place']),
+    'county': set(['county', 'parish', 'census_area', 'borough', 'region']),
+    'parish': set(['county', 'parish', 'census_area', 'borough', 'region']),
+    'census_area': set([
+        'county',
+        'parish',
+        'census_area',
+        'borough',
+        'region']),
+    'borough': set(['county', 'parish', 'census_area', 'borough', 'region']),
+    'region': set(['county', 'parish', 'census_area', 'borough', 'region']),
+    'state': set(['state', 'territory', 'district']),
+    'territory': set(['state', 'territory', 'district']),
+    'council': set(['council_district', 'commissioner_district']),
+    'commissioner': set(['council_district', 'commissioner_district']),
+    'court': set([
+        'chancery_court',
+        'superior_court',
+        'supreme_court',
+        'court_of_appeals',
+        'district_court',
+        'circuit_court',
+        'constable_districts']),
+    'school': set(['school_board']),
+    'ward': set(['anc', 'ward', 'precinct']),
+    'anc': set(['anc', 'ward', 'precinct']),
+    'precinct': set(['anc', 'ward', 'precinct']),
+    'country': set(['country']),
+    'cd': set(['cd']),
+    'sldl': set(['sldl']),
+    'sldu': set(['sldu'])}
+
 # Old unnecessarily fancy code, kept if we decide to revert to a recursive
 # matched model later
 """
@@ -64,6 +77,7 @@ def create_ocdid_dict(list_vals,ocd_dict):
         ocd_dict[val] = {}
     create_ocdid_dict(list_vals,ocd_dict[val])
 """
+
 
 def is_ocdid(ocdid):
     """Check whether given ocdid is contained in the official ocdid list
@@ -81,6 +95,7 @@ def is_ocdid(ocdid):
     else:
         return False
 
+
 def is_exception(ocdid):
     """Check whether given ocdid is contained in the exception list
 
@@ -97,6 +112,7 @@ def is_exception(ocdid):
     else:
         return False
 
+
 def get_exception(ocdid):
     """Returns official ocdid if ocdid value is in exceptions
 
@@ -112,7 +128,8 @@ def get_exception(ocdid):
         return exceptions[ocdid]
     return None
 
-def match_name(ocdid_prefix,dist_type,dist_name):
+
+def match_name(ocdid_prefix, dist_type, dist_name):
     """Given a district name, returns closest ocdid match in given district
 
     Keyword arguments:
@@ -127,26 +144,27 @@ def match_name(ocdid_prefix,dist_type,dist_name):
     """
     # from list of districts of a given type, find the closest match
     try:
-        match = process.extractOne(dist_name,ocdids[ocdid_prefix][dist_type])
+        match = process.extractOne(dist_name, ocdids[ocdid_prefix][dist_type])
     except KeyError:
         print 'Invalid ocdid_prefix or dist_type provided'
         raise
 
     # if match fails, return empty values
     if not match:
-        return None,-1
-    id_val,ratio = match
+        return None, -1
+    id_val, ratio = match
 
     # format ocdid, check that it exists, return id value and match ratio
-    ocdid = '{}/{}:{}'.format(ocdid_prefix,dist_type,id_val)
+    ocdid = '{}/{}:{}'.format(ocdid_prefix, dist_type, id_val)
     if is_ocdid(ocdid):
-        return ocdid,ratio
+        return ocdid, ratio
     elif is_exception(ocdid):
-        return get_exception(ocdid),ratio
+        return get_exception(ocdid), ratio
     else:
-        return None,-1
+        return None, -1
 
-def match_type(ocdid_prefix,dist_type,dist_count):
+
+def match_type(ocdid_prefix, dist_type, dist_count):
     """Given a district type and count, returns official ocdid district type
 
     Keyword arguments:
@@ -166,39 +184,43 @@ def match_type(ocdid_prefix,dist_type,dist_count):
     diff_len = 1000
     type_ratio = 0
 
-    for k,v in ocdids[ocdid_prefix].iteritems():
-        #special case for school based districts, must be explicitly requested
+    for k, v in ocdids[ocdid_prefix].iteritems():
+        # special case for school based districts, must be explicitly requested
         if 'school' in k and dist_type != 'school':
             continue
         # matches to district closest in count, using district type as a
         # secondary matching trait, 'district' is the generic type
-        new_diff_len = abs(len(v)-dist_count)
-        new_type_ratio = fuzz.ratio(k,dist_type)
+        new_diff_len = abs(len(v) - dist_count)
+        new_type_ratio = fuzz.ratio(k, dist_type)
         if new_diff_len < diff_len:
             diff_len = new_diff_len
             key = k
             type_ratio = new_type_ratio
-        elif new_diff_len == diff_len and dist_type != 'district' and new_type_ratio > type_ratio:
+        elif (
+                new_diff_len == diff_len and
+                dist_type != 'district' and
+                new_type_ratio > type_ratio
+        ):
             key = k
             type_ratio = new_type_ratio
 
     # district length difference must be less than 5% for a valid match
     if diff_len == 0:
         return key
-    elif float(diff_len)/dist_count < .05:
+    elif float(diff_len) / dist_count < .05:
         ocd_count = len(ocdids[ocdid_prefix][key])
         if dist_count > ocd_count:
-            print 'Extra provided districts:{}'.format(dist_count-ocd_count)
+            print 'Extra provided districts:{}'.format(dist_count - ocd_count)
         else:
-            print 'Extra official districts:{}'.format(ocd_count-dist_count)
+            print 'Extra official districts:{}'.format(ocd_count - dist_count)
         return key
     else:
         return 'No match'
 
+
 def name_search(name):
     """Given a district name, searches for all matching ocdids
-        
-    ***SLOW MATCHING*** 
+    ***SLOW MATCHING***
     searches everything, use a more limiting search for quicker results
 
     Keyword arguments:
@@ -210,21 +232,25 @@ def name_search(name):
 
     """
     match_list = []
-    
-    for prefix,district in ocdids.iteritems():
-        for dist_type,dist_names in district.iteritems():
+
+    for prefix, district in ocdids.iteritems():
+        for dist_type, dist_names in district.iteritems():
             # pull the closest match from each set of districts, adds to
             # match_list if > MATCH_RATIO
-            match_vals = process.extractOne(name,dist_names)
+            match_vals = process.extractOne(name, dist_names)
             if match_vals and match_vals[1] > MATCH_RATIO:
-                match_list.append((match_vals[1],'{}/{}:{}'.format(prefix,dist_type,match_vals[0])))
-    
+                match_list.append(
+                    (match_vals[1], '{}/{}:{}'.format(prefix,
+                                                      dist_type,
+                                                      match_vals[0])))
+
     # sorts and returns top MATCH_LIMIT matches
-    match_list = sorted(match_list,key=itemgetter(0))
+    match_list = sorted(match_list, key=itemgetter(0))
     match_list.reverse()
     return match_list[:MATCH_LIMIT]
 
-def type_name_search(type_val,name):
+
+def type_name_search(type_val, name):
     """Given a district name and type, searches for all matching ocdids
 
     Keyword arguments:
@@ -236,7 +262,7 @@ def type_name_search(type_val,name):
 
     Returns:
     match_list[:MATCH_LIMIT] -- a list of the top 'MATCH_LIMIT' matches that
-                                    are greater than 'MATCH_RATIO' 
+                                    are greater than 'MATCH_RATIO'
 
     """
     match_list = []
@@ -248,17 +274,20 @@ def type_name_search(type_val,name):
     else:
         valid_dists == 'all'
 
-    for prefix,district in ocdids.iteritems():
-        for dist_type,dist_names in district.iteritems():
+    for prefix, district in ocdids.iteritems():
+        for dist_type, dist_names in district.iteritems():
             # pull the closest match from matching sets of districts, adds to
             # match_list if > MATCH_RATIO
             if valid_dists == 'all' or dist_type in valid_dists:
-                match_vals = process.extractOne(name,dist_names)
+                match_vals = process.extractOne(name, dist_names)
                 if match_vals and match_vals[1] > MATCH_RATIO:
-                    match_list.append((match_vals[1],'{}/{}:{}'.format(prefix,dist_type,match_vals[0])))
+                    match_list.append(
+                        (match_vals[1], '{}/{}:{}'.format(
+                            prefix, dist_type,
+                            match_vals[0])))
 
     # sorts and returns top MATCH_LIMIT matches
-    match_list = sorted(match_list,key=itemgetter(0))
+    match_list = sorted(match_list, key=itemgetter(0))
     match_list.reverse()
     return match_list[:MATCH_LIMIT]
 
@@ -271,15 +300,15 @@ def print_subdistrict_data(ocdid_prefix):
 
     """
     print ocdid_prefix
-    for k,v in ocdids[ocdid_prefix].iteritems():
-        print '  - {}:{}'.format(k,v)
+    for k, v in ocdids[ocdid_prefix].iteritems():
+        print '  - {}:{}'.format(k, v)
 
 """ If a url is provided, use 'requests' to obtain ocdid data
         otherwise, read from file system """
 if 'http' in OCDID_DATA:
     r = requests.get(OCDID_DATA)
 else:
-    f = open(OCDID_DATA,'r')
+    f = open(OCDID_DATA, 'r')
     r = f.read()
 
 """ Generate a set of only ocdid data with empty values removed """
@@ -293,14 +322,18 @@ ocdids = {}
 if 'http' in EXCEPTION_DATA:
     r = requests.get(EXCEPTION_DATA)
 else:
-    f = open(EXCEPTION_DATA,'r')
+    f = open(EXCEPTION_DATA, 'r')
     r = f.read()
 
 exceptions = {}
 for line in r.text.split('\n'):
     line = line.split(',')
     # Ignore exception that indicate nonexistant districts
-    if len(line) > 2 and line[0][:line[0].rfind(':')] != line[1][:line[0].rfind(':')] and not line[2].startswith('Doesn\'t exist'): 
+    if (
+            len(line) > 2 and line[0][:line[0].rfind(':')]
+            !=
+            line[1][:line[0].rfind(':')]
+            and not line[2].startswith('Doesn\'t exist')):
         exceptions[line[0]] = line[1]
 
 """ Create a dictionary of ocdid data in the format:
@@ -310,12 +343,12 @@ for line in r.text.split('\n'):
                 district_type:
                     [name_1,name_2,etc.]
             }
-        } 
+        }
 """
 for ocdid in ocdid_set:
     prefix_div = ocdid.rfind('/')
     ocdid_prefix = ocdid[:prefix_div]
-    type_val,name = ocdid[prefix_div+1:].split(':')
+    type_val, name = ocdid[prefix_div + 1:].split(':')
     if ocdid_prefix not in ocdids:
         ocdids[ocdid_prefix] = {}
     if type_val not in ocdids[ocdid_prefix]:
@@ -325,7 +358,7 @@ for ocdid in ocdid_set:
 for ocdid in exceptions.keys():
     prefix_div = ocdid.rfind('/')
     ocdid_prefix = ocdid[:prefix_div]
-    type_val,name = ocdid[prefix_div+1:].split(':')
+    type_val, name = ocdid[prefix_div + 1:].split(':')
     if ocdid_prefix not in ocdids:
         ocdids[ocdid_prefix] = {}
     if type_val not in ocdids[ocdid_prefix]:
