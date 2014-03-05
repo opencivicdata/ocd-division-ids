e7d4641eb28e1e1161caf20aad0d6a5204e67170
James Turk <james.p.turk@gmail.com>
2014-03-05 16:03:58 -0500

    do everything that verify used to do, but now with headers

--- scripts/verify.py
+++ scripts/verify.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import glob
 import argparse
+import warnings
 import collections
 
 # Explicitly disallow python 2.x
@@ -12,95 +13,95 @@ if sys.version_info < (3, 0):
     sys.stdout.write("Python 2.x not supported.\n")
     sys.exit(1)
 
-VALID_ID_IGNORE_CASE = re.compile(r'^ocd-division/country:[a-z]{2}(/[^\W\d]+:[\w.~-]+)*$', re.U)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='verify published CSV files')
-    parser.add_argument('country', type=str, default=None, help='country to verify')
+def validate_id(id_):
+    id_regex = re.compile(r'^ocd-division/country:[a-z]{2}(/[^\W\d]+:[\w.~-]+)*$', re.U)
+    if not id_regex.match(id_) and id_.lower() == id_:
+        raise ValueError('invalid id: ' + id_)
+
+
+def open_csv(filename):
+    print('processing', filename)
+    fh = open(filename)
+    first_row = next(csv.reader(fh))
+    if 'ocd-division/country' in first_row[0]:
+        warnings.warn('proceeding in legacy mode, please add column headers to file',
+                      DeprecationWarning)
+        fh.seek(0)
+        return csv.DictReader(fh, ('division_id', 'name'))
+    else:
+        fh.seek(0)
+        return csv.DictReader(fh)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='combine component CSV files into one')
+    parser.add_argument('country', type=str, default=None, help='country to compile')
     args = parser.parse_args()
-
     country = args.country.lower()
 
-    ids = collections.defaultdict(list)
-    seen_parents = set()
-    invalid_ids = set()
+    ids = collections.defaultdict(dict)
+    sources = collections.defaultdict(list)
+    all_keys = []
+    missing_parents = set()
     types = collections.Counter()
-    all_rows = []
 
-    duplicates = 0
-
-    for filename in glob.glob('identifiers/country-{0}/*.csv'.format(country)):
+    for filename in glob.glob('identifiers/country-{}/*.csv'.format(country)):
         if filename.endswith('exceptions.csv'):
             continue
-        print('processing', filename)
-        for id_, name in csv.reader(open(filename)):
-            all_rows.append((id_, name))
 
-            # check for dupes
-            if id_ in ids:
-                duplicates += 1
-                print('Duplicate ID {0} seen in {1}, and {2}'.format(
-                    id_, ', '.join(ids[id_]), filename))
-            ids[id_].append(filename)
+        csvfile = open_csv(filename)
+        for field in csvfile.fieldnames:
+            if field not in all_keys:
+                all_keys.append(field)
 
-            if not VALID_ID_IGNORE_CASE.match(id_):
-                invalid_ids.add(id_)
-
-            # check that all characters are lowercase
-            if id_.lower() != id_:
-                invalid_ids.add(id_)
+        for row in csvfile:
+            id_ = row['division_id']
+            validate_id(id_)
 
             # check parents
             parent, endpiece = id_.rsplit('/', 1)
-            if parent != 'ocd-division':
-                seen_parents.add(parent)
-
+            if parent != 'ocd-division' and parent not in ids:
+                missing_parents.add(parent)
             # count types
             type_ = endpiece.split(':')[0]
             types[type_] += 1
 
-    print('{0} duplicates'.format(duplicates))
+            # update record
+            id_record = ids[id_]
+            for key, val in row.items():
+                if key not in id_record:
+                    id_record[key] = val
+                elif id_record[key] != val:
+                    print('mismatch for attribute {} on {} from {}'.format(key, id_, filename))
+                    print('other sources:')
+                    for source in sources[id_]:
+                        print('   ', source)
+                    return 1
+            # add source
+            sources[id_].append(filename)
 
-    seen_parents -= set(ids.keys())
-    print('{0} unknown parents'.format(len(seen_parents)))
-    for parent in sorted(seen_parents):
-        print('   ', parent)
+    missing_parents -= set(ids.keys())
+    if missing_parents:
+        print(len(missing_parents), 'unknown parents')
+        for parent in sorted(seen_parents):
+            print('   ', parent)
+        return 1
 
-    print('{0} invalid ids'.format(len(invalid_ids)))
-    for i in sorted(invalid_ids):
-        print('   ', i)
-
-    print('stats')
+    print('types')
     for type_, count in types.most_common():
         print('   ', type_, count)
 
+    # TODO: track % of records with each type & output here
+
     # write output file
-    if not duplicates and not seen_parents:
-        with open('identifiers/country-{0}.csv'.format(country), 'w') as out:
-            out = csv.writer(out)
-            for row in sorted(all_rows):
-                out.writerow(row)
-    else:
-        print('NO COUNTRY CSV FILE WRITTEN')
+    output_file = 'identifiers/country-{}.csv'.format(country)
+    print('writing', output_file)
+    with open(output_file, 'w') as out:
+        out = csv.DictWriter(out, fieldnames=all_keys)
+        out.writeheader()
+        for id_, row in sorted(ids.items()):
+            out.writerow(row)
 
-    # do geoid validation too (TODO: add a flag for this)
-    seen_in_geoid = set()
-    all_geo_rows = list()
-    if country == 'us':
-        for filename in glob.glob('mappings/us-census-geoids/*.csv'):
-            for id_, geoid in csv.reader(open(filename)):
-                seen_in_geoid.add(id_)
-                if id_ not in ids:
-                    print('unexpected geoid for', id_)
-                all_geo_rows.append((id_, geoid))
-
-        #unknown_ids = set(ids.keys()) - seen_in_geoid
-        #if not unknown_ids:
-        #    print('no missing geoids!')
-        #else:
-        #    for id in sorted(unknown_ids):
-        #        print('missing geoid for id', id)
-        with open('mappings/us-census-geoids.csv', 'w') as out:
-            out = csv.writer(out)
-            for row in sorted(all_geo_rows):
-                out.writerow(row)
+if __name__ == '__main__':
+    sys.exit(main())
