aa5385f09f80095fb4b1faf97f2d8ea62891aa8a
Paul Tagliamonte <tag@pault.ag>
2014-03-11 14:43:05 -0400

    Fix up a few bad generated IDs.

--- identifiers/country-us/openstates/tools/autogenerate.py
+++ identifiers/country-us/openstates/tools/autogenerate.py
@@ -5,14 +5,10 @@ import csv
 
 # In case you've not guessed, this is a one-off to help.
 OPENSTATES_CSV_ROOT = "/home/tag/dev/sunlight/openstates/manual_data/districts"
-DISTRICT_INFO = re.compile("(?P<flavor>.*)/(?P<state>.*)-(?P<district>.*)")
+DISTRICT_INFO = re.compile("(?P<flavor>.*)/(?P<state>[^-]*)-(?P<district>.*)")
 
-STATES = ["ma.csv",
-          "vt.csv",
-          "sc.csv",
-          "nh.csv",
-          "md.csv",]
-# STATES = os.listdir(OPENSTATES_CSV_ROOT)
+# STATES = ["vt.csv", "ma.csv"]
+STATES = os.listdir(OPENSTATES_CSV_ROOT)
 
 for state in STATES:
     state_, _ = state.split(".", 1)
@@ -30,8 +26,14 @@ for state in STATES:
                 info = DISTRICT_INFO.match(boundary_id).groupdict()
                 print(info)
 
-                gid = "ocd-division/country:us/state:%s/%s:%s" % (
-                    abbr, info['flavor'], info['district'],)
+                abbr_type = "state"
+                if abbr == "pr":
+                    abbr_type = "territory"
+                if abbr == "dc":
+                    abbr_type = "district"
+
+                gid = "ocd-division/country:us/%s:%s/%s:%s" % (
+                    abbr_type, abbr, info['flavor'], info['district'],)
 
                 wfd.write("%s,%s\n" % (gid, "%s-%s-%s" % (
                     abbr, chamber, info['district'])))
