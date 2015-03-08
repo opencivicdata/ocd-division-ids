3f90cf7270b9c7f142b53269a0cb11f6eba6aadd
James McKinney <james@slashpoundbang.com>
2015-03-08 17:53:15 -0400

    fix montreal borough and district names

--- scripts/country-ca/ca_qc_montreal_boroughs.rb
+++ scripts/country-ca/ca_qc_montreal_boroughs.rb
@@ -19,26 +19,27 @@ class Montreal < Runner
   def names
     puts CSV.generate_line(%w(id name abbreviation))
     csv.map do |row|
-      name = row['Nom-officiel'].gsub("–", "—").gsub('’', "'") # n-dash to m-dash
-      output("csd:2466023/borough:", row['No-arro-election'], name, row['Code3L'])
+      output("csd:2466023/borough:", row['No-arro-election'], clean_name(row['Nom-officiel']), row['Code3L'])
     end
   end
 
   def urls
     map = {}
     csv.each do |row|
-      name = row['Nom-officiel'].gsub("–", "—").gsub('’', "'") # n-dash to m-dash
-      map[name] = row['No-arro-election']
+      map[clean_name(row['Nom-officiel'])] = row['No-arro-election']
     end
 
     puts CSV.generate_line(%w(id url))
     Nokogiri::HTML(open("http://ville.montreal.qc.ca/portal/page?_pageid=5798,85813661&_dad=portal&_schema=PORTAL")).css("#nav_coll a").map do |a|
-      name = a.text.gsub("–", "—").gsub('’', "'") # n-dash to m-dash
-      output("csd:2466023/borough:", map.fetch(name), "http://ville.montreal.qc.ca#{a[:href]}")
+      output("csd:2466023/borough:", map.fetch(clean_name(a.text)), "http://ville.montreal.qc.ca#{a[:href]}")
     end
   end
 
 private
+
+  def clean_name(name)
+    name.gsub("–", "—").gsub('’', "'").sub(/Le /, '') # n-dash to m-dash
+  end
 
   def csv
     # The shapefile from Montreal's open data portal contains no accents.
