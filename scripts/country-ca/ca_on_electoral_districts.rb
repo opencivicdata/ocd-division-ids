3177d389dab86366ada84b6b825e5e12894493b8
James McKinney <james@slashpoundbang.com>
2016-08-02 00:11:59 -0400

    ca: Fix ON script

--- scripts/country-ca/ca_on_electoral_districts.rb
+++ scripts/country-ca/ca_on_electoral_districts.rb
@@ -7,10 +7,13 @@ require File.expand_path(File.join("..", "utils.rb"), __FILE__)
 
 class ON < Runner
   def names
-    puts CSV.generate_line(%w(id name))
-    Nokogiri::HTML(open("http://fyed.elections.on.ca/fyed/en/list_page_en.jsp?show=all")).xpath("//table[@width=600]//a").sort_by{|a| a[:href]}.each do |a|
-      output("province:on/ed:", a[:href][%r{=0*(\d+)\z}, 1], UnicodeUtils.downcase(a.text.sub('CHATHAM--KENT--', 'CHATHAM-KENT--').gsub('--', '—')).gsub(/\b(?!(?:and|s|the)\b)(\w)/){UnicodeUtils.upcase($1)})
-    end
+    ShapefileParser.new(
+      "http://www.elections.on.ca/content/dam/NGW/sitecontent/2016/preo/shapefiles/Electoral%20District%20Shapefile.zip",
+      "province:on/ed:", {
+        :id => "ED_ID",
+        :name => lambda{|record| UnicodeUtils.downcase(record.attributes["ENGLISH_NA"].sub('CHATHAM--KENT--', 'CHATHAM-KENT--').gsub('--', '—')).gsub(/\b(?!(?:and|s|the)\b)(\w)/){UnicodeUtils.upcase($1)}},
+      }
+    ).run
   end
 end
 
