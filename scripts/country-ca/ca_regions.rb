38d4277ba8817f620de46349984b8459200e65b3
James McKinney <james@slashpoundbang.com>
2017-02-23 21:46:45 -0500

    ca: Update ca_regions.rb

--- scripts/country-ca/ca_regions.rb
+++ scripts/country-ca/ca_regions.rb
@@ -3,7 +3,7 @@
 
 require File.expand_path(File.join("..", "utils.rb"), __FILE__)
 
-# Scrapes region codes and names from statcan.gc.ca
+# Scrapes region codes and names from Statistics Canada
 
 class Regions < Runner
   def initialize(*args)
@@ -29,7 +29,7 @@ private
   def rows(column_name, language)
     puts CSV.generate_line(['id', column_name])
     # The regions appear in the same order in both languages.
-    Nokogiri::HTML(open("http://www23.statcan.gc.ca/imdb/p3VD#{language}.pl?Function=getVDPage1&db=imdb&dis=2&adm=8&TVD=116940")).xpath("//tbody/tr").each do |tr|
+    Nokogiri::HTML(open("http://www23.statcan.gc.ca/imdb/p3VD#{language}.pl?Function=getVD&TVD=314300")).xpath("//tbody/tr").each do |tr|
       output("region:", tr.at_xpath('./th[1]/a//text()').text, tr.at_xpath('./td[1]').text) # the number is the first digit of any SGC code
     end
   end
