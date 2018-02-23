d3317c129ff1771dbf9f643c0fbb241fd7267c42
Tim Showers <showerst@gmail.com>
2018-02-22 19:49:07 -0500

    IE: add base, upper and lower parliament divisions

---
+++ scripts/country-ie/oireachtas.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import csv
+import re
+
+# fetch the lower / upper houses of the Oreichtas from the IE api
+# https://api.oireachtas.ie/v1/constituencies?chamber=dail
+# https://api.oireachtas.ie/v1/constituencies?chamber=seanad
+
+def make_id(type_id):
+    replacements = []
+    for replacement in replacements:
+        type_id = type_id.replace(replacement, '')
+
+    type_id = type_id.lower()
+    type_id = re.sub('\.? ', '_', type_id)
+    type_id = re.sub('[^\w0-9~_.-]', '~', type_id, re.UNICODE)
+    return type_id
+
+def write_csv(filename, csv_columns, dict_data):
+    with open(filename, 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in dict_data:
+            writer.writerow(data)
+
+
+
+ocd_base = 'ocd-division/country:ie'
+
+rows = []
+for house in ['dail','seanad']:
+    url = 'https://api.oireachtas.ie/v1/constituencies?chamber={}'.format(house)
+
+    req = requests.get(url)
+    res = json.loads(req.content)
+    for district in res['results']['house']['constituenciesOrPanels']:
+        row = {}
+        name = district['constituencyOrPanel']['showAs']
+        local_id = make_id(name)
+
+        # lower / dail
+        if district['constituencyOrPanel']['representType'] == 'constituency':
+            row['id'] = '{}/constituency:{}'.format(ocd_base, local_id)
+        elif district['constituencyOrPanel']['representType'] == 'panel':
+            row['id'] = '{}/panel:{}'.format(ocd_base, local_id)
+
+        row['name'] = name
+        rows.append(row)
+
+csv_filename = 'identifiers/country-ie/oireachtas.csv'
+write_csv(csv_filename, rows[0].keys(), rows)
