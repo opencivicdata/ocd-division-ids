79faad0f8d44eda648151deb948496a4f79fc913
Rahul Nath <nathrahul@google.com>
2019-10-18 17:32:10 -0400

    Add scripts and csv files

--- scripts/country-in/create_ocd_ids.py
+++ scripts/country-in/create_ocd_ids.py
@@ -1,8 +1,6 @@
-from __future__ import print_function
-import csv
 """
 Program to generate OCD IDs for the 2019 Indian Vidhan Sabha elections.
-
+Adaptable to generate OCD-IDs for elections with districts and constituencies.
 State OCD-IDs created for Bihar, Delhi, Jharkhand, Maharashstra, and Haryana.
 
 Information on Districts and Constituencies for each state are read from a
@@ -17,15 +15,32 @@ The {state_abbreviation}_constituencies.csv files can be created using the
 This script writes OCD-IDs into a CSV with the election name, in this case
 vidhan_sabha.csv. It also prints out the parents OCD-IDs that need to be added
 to the `federal_states_territories.csv` file before `compile.py` is run.
-"""
 
-state_abbrevs = {"hr": "Haryana", "mh": "Maharashtra", "br": "Bihar", "dl": "Delhi", "jh": "Jharkhand"}
-columns = ["id", "name"]
+Source of truth for district and constituency names: https://electoralsearch.in
+"""
+from __future__ import print_function
+import csv
+
+
+# For file names and OCD-IDs.
+state_abbrevs = {"hr": "Haryana", "mh": "Maharashtra",
+                 "br": "Bihar", "dl": "Delhi", "jh": "Jharkhand"}
 country = "in"
+
+# Final output CSV name and its columns.
 election_name = "Vidhan Sabha"
+columns = ["id", "name"]
+
+# Final table being written to ocd-id CSV
 write_table = []
 new_file = True
 
+# If top level OCD-IDs don't exist
+# Prints them to stdout to be added to
+# federal_states_territories.csv.
+print_top_level_ids = True
+
+# cleans the data of certain punctuation
 punc_replacements = {
     " ": "_",
     "(": "",
@@ -34,24 +49,22 @@ punc_replacements = {
 }
 
 def read_csv(csv_file):
-  # returns in-memory list of table rows
+  """Returns in-memory list of table rows."""
   table = []
-  with open("csv/" + csv_file, "rb") as f:
+  with open("csv/" + csv_file, "r", encoding="utf-8") as f:
       csv_reader = csv.DictReader(f)
       for row in csv_reader:
         table.append(row)
   return table
 
 def join_table(consts, districts, state, state_abbr):
-  # return joined table
+  """Joins abbrevations with constituency districts."""
   new_table = []
-  ht = {}
+  ht = {} # Hashtable that stores the district abbreviation.
   for d_row in districts:
     district_key = d_row["district"]
     ht[district_key] = d_row["abbreviation"].lower()
   for c_row in consts:
-    # source of truth on district names:
-    # https://affidavit.eci.gov.in/
     cons_district = c_row["district"].strip()
 
     if cons_district in ht:
@@ -71,41 +84,50 @@ def join_table(consts, districts, state, state_abbr):
   return new_table
 
 def write_to_file(table):
-  # format hardcoded OCD ID
+  """Formats hardcoded OCD ID and writes to CSV file."""
   global new_file
   ocd_id = "ocd-division/country:{}/{}:{}/district:{}/cd:{}"
   rest = "{} constituency; {} district; {}"
   write_header = None
-  # used to create top level OCD IDs if they don't exist
+
+  # Used to create top level OCD IDs if they don't exist.
   parent_set = set()
+
+  # Write headers if it's the first table being written.
   if new_file:
     open_type = "w+"
     new_file = False
     write_header = True
   else:
     open_type = "a+"
-  with open("{}.csv".format(election_name.lower().replace(" ", "_")), open_type) as f:
+  with open("{}.csv".format(election_name.lower().replace(" ", "_")),
+            open_type, encoding="utf-8") as f:
     writer = csv.DictWriter(f, fieldnames=columns)
     if write_header:
       writer.writeheader()
       write_header = False
     for row in table:
+      # An exception: Delhi is a territory, not a state.
       if row["state_abbr"] == "dl":
         state_name = "territory"
       else:
         state_name = "state"
-      full_const = " ".join(word.capitalize() for word in row["constituency"].split("_"))
+      full_const = " ".join(word.capitalize()
+                            for word in row["constituency"].split("_"))
       ocd_id_row = {
           "id": ocd_id.format(country, state_name, row["state_abbr"],
                               row["district_abbr"], row["constituency"]),
           "name": rest.format(full_const, row["district"], row["state"])
       }
-      parent = ocd_id_row["id"].rsplit("/", 1)
-      parent_set.add("/".join(parent[:-1]) + "," + row["district"])
+      if print_top_level_ids:
+        parent = ocd_id_row["id"].rsplit("/", 1)
+        parent_set.add("/".join(parent[:-1]) + "," + row["district"])
       writer.writerow(ocd_id_row)
-  for parent in sorted(parent_set, key=lambda x: x.split(",")[-1]):
-    print(parent)
+  if print_top_level_ids:
+    for parent in sorted(parent_set, key=lambda x: x.split(",")[-1]):
+      print(parent)
 
+# Driver script
 for state_abbr, state  in sorted(state_abbrevs.items()):
   consts = read_csv("{}_constituencies.csv".format(state_abbr))
   districts = read_csv("{}_districts.csv".format(state_abbr))
--- scripts/country-in/fetch_constituencies.py
+++ scripts/country-in/fetch_constituencies.py
@@ -1,21 +1,41 @@
+"""
+This script fetches districts and constituencies for Indian states from
+the official Indian electoral site for Vidhan Sabha elections. It then
+outputs constituencies and districts for each state in a CSV file named
+corresponding to each state. From these CSVs, one may run the `create_ocd_id.py`
+script to generate OCD-IDs (after separately creating district abbreviation
+files for each state).
+
+The official site is https://electoralsearch.in
+"""
 import urllib.request
 import csv
 import json
 
+# Codes gotten from official site.
 state_codes = { "br": "S04", "dl": "U05", "jh": "S27", "hr": "S07", "mh": "S13" }
 consts_file = "{}_constituencies"
 districts_url = "https://electoralsearch.in/Home/GetDistList?st_code={}"
 constituency_url = "https://electoralsearch.in/Home/GetAcList?dist_no={}&st_code={}"
-replacements = {
-    "_": " ",
-    "-": ""
-}
+
+# CSV columns
 columns = ["constituency", "district"]
+
+# Any specific data-cleaning replacements that should be done
+# should be added here.
 name_replacements = {
     "Kaimur (bhabua)".upper(): "Kaimur",
     "saraikela- kharswan".upper(): "Saraikela Kharswan"
 }
 
+replacements = {
+    "_": " ",
+    "-": ""
+}
+
+state_consts = {}
+
+# Cleans up each a given column cell.
 def clean_name(name):
   name = name.strip()
   name_test = name.split(" ")
@@ -28,32 +48,36 @@ def clean_name(name):
       name = name.replace(old, new)
     return " ".join([n.lower().capitalize() for n in name.split(" ")])
 
-
-state_consts = {}
-
+# Driver script
 for state, state_code in state_codes.items():
+  # Get each state's district data
   with urllib.request.urlopen(districts_url.format(state_code)) as response:
     data = response.read()
     encoding = response.info().get_content_charset("utf-8")
     res = json.loads(data.decode(encoding))
   const_districts = {}
-  for district in res:
 
+  # From each district get information to fetch its constituencies.
+  for district in res:
     dist_code = district["dist_no"]
     init_name = district["dist_name"]
     if state == "dl" and "delhi" not in district["dist_name"].lower():
       init_name = init_name.replace(" ", "") + " DELHI"
     district_name = clean_name(init_name)
-    with urllib.request.urlopen(constituency_url.format(dist_code, state_code)) as const_response:
+    with urllib.request.urlopen(constituency_url.format(
+        dist_code, state_code)) as const_response:
       data = const_response.read()
       encoding = const_response.info().get_content_charset("utf-8")
       const_res = json.loads(data.decode(encoding))
+
+    # create dictionary of constituencies to their districts.
     for const in const_res:
       const_name = clean_name(const["ac_name"])
       const_districts[const_name] = district_name
   state_consts[state] = const_districts
 
 
+# For each state's constituency, write them to a state specific CSV
 for state, consts in sorted(state_consts.items()):
   with open("{}_constituencies.csv".format(state), "w+") as f:
     writer = csv.DictWriter(f, fieldnames=columns)
