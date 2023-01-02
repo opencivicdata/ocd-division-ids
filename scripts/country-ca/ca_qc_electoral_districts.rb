ddaf5c7b82c6538b635ef730b24b2d0c979080dd
Simon Meers <simon@simonmeers.com>
2023-01-02 15:04:05 +1100

    I don't think we need to maintain two separate versions going forward?

--- scripts/country-ca/ca_qc_electoral_districts.rb
+++ scripts/country-ca/ca_qc_electoral_districts.rb
@@ -6,16 +6,6 @@ require File.expand_path(File.join("..", "utils.rb"), __FILE__)
 # Scrapes Quebec electoral district codes and names from electionsquebec.qc.ca
 
 class QC < Runner
-  def initialize(*args)
-    super
-
-    add_command({
-      :name        => "names-2022",
-      :description => "Prints a CSV of identifiers and English names",
-      :output_path => "identifiers/country-ca/province-qc-electoral_districts-2022.csv",
-    })
-  end
-
   def names
     puts CSV.generate_line(%w(id name))
     # No official government source has a full list of identifiers and names
@@ -25,16 +15,17 @@ class QC < Runner
     end
   end
 
-  def names_2022
-    ShapefileParser.new(
-      "https://www.dgeq.org/circonscriptions_electorales_2022_shapefile.zip",
-      "province:qc/ed:", {
-        :id => lambda{|record| "#{record.attributes["CO_CEP"]}-2022"},
-        :name => "NM_CEP",
-        :validFrom => lambda{|record| "2022-10-03"},
-      }
-    ).run
-  end
+  # I don't think we need this?
+  # def names_2022
+  #   ShapefileParser.new(
+  #     "https://www.dgeq.org/circonscriptions_electorales_2022_shapefile.zip",
+  #     "province:qc/ed:", {
+  #       :id => lambda{|record| "#{record.attributes["CO_CEP"]}-2022"},
+  #       :name => "NM_CEP",
+  #       :validFrom => lambda{|record| "2022-10-03"},
+  #     }
+  #   ).run
+  # end
 end
 
 QC.new("province-qc-electoral_districts.csv").run(ARGV)
